5f0aa00de52e9e36e340e739b3005a433f067775
c0rdial <0132601@kdu-online.com>
2023-10-18 12:14:06 +0800

    package and add jest.config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, waitFor, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockFetch = jest.fn();
+
+global.fetch = mockFetch;
+
+
+describe('App', () => {
+  beforeEach(() => {
+    mockFetch.mockClear();
+  });
+
+  it('fetches and displays notes', async () => {
+    const mockNotes = [
+      { id: 1, title: 'Test Title', content: 'Test Content' }
+    ];
+    mockFetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockNotes)
+    });
+
+    render(<App />);
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
+
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+    expect(screen.getByText('Test Content')).toBeInTheDocument();
+  });
+
+  it('updates title and content when a note is clicked', async () => {
+    const mockNotes = [
+      { id: 1, title: 'Test Title', content: 'Test Content' }
+    ];
+    mockFetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockNotes)
+    });
+
+    render(<App />);
+    await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
+
+    fireEvent.click(screen.getByText('Test Title'));
+  });
 });
